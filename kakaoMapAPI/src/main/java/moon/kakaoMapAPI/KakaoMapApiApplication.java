41200213c71437bfb4e9d02d1be14d6a5ea49302
ExpeditionMoon <dnjswon1025@gmail.com>
2024-05-04 16:51:09 +0900

    chore: 프로젝트 생성

---
+++ kakaoMapAPI/src/main/java/moon/kakaoMapAPI/KakaoMapApiApplication.java
@@ -0,0 +1,13 @@
+package moon.kakaoMapAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoMapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoMapApiApplication.class, args);
+	}
+
+}
---
+++ kakaoMapAPI/src/test/java/moon/kakaoMapAPI/KakaoMapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package moon.kakaoMapAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoMapApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
