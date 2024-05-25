cd79c26feacf0610b32d80d00b8139c04cf8f9da
ExpeditionMoon <dnjswon1025@gmail.com>
2024-05-25 22:58:56 +0900

    chore(recipe): 프로젝트 생성 및 레시피 Api 연결
    - 레시피 Open Api 연결 로직 구현
    
    #7

---
+++ recipeAndCart/src/test/java/moon/recipeAndCart/RecipeAndCartApplicationTests.java
@@ -0,0 +1,13 @@
+package moon.recipeAndCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAndCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
